978024ccf73a92a54c395a5a8abb216a5912b7ab
adriensoulie <minishoes31400@gmail.com>
2021-02-02 18:43:28 +0100

    added geolocalisation feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Weather from './component/Weather'
 import cityWeatherApi from './api/cityWeatherApi'
 import './App.css';
@@ -7,6 +7,37 @@ function App() {
   const [city, setCity] = useState('')
   const [error, setError] = useState(false)
   const [weather, setWeather] = useState('')
+  const [loading, isLoading] = useState(false)
+  
+
+  useEffect(() => {
+
+    if( navigator.geolocation){
+
+      const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=replace_lat&lon=replace_lon&appid=3e0125ad1c01dc42cced5e471fbc8f4b'
+  
+      navigator.geolocation.getCurrentPosition(function(pos){
+        isLoading(true)
+        //'pos' return object has many properties we can grab
+        var geoLat = pos.coords.latitude.toFixed(5);
+        var geoLng = pos.coords.longitude.toFixed(5);
+  
+  
+        const urlLon = baseUrl.replace('replace_lon', geoLng)
+        const fetchUrlLocalisation = urlLon.replace('replace_lat', geoLat)
+
+        fetch(fetchUrlLocalisation)
+          .then((res)=> res.json())
+          .then((res => {
+            setWeather(res)
+            isLoading(false)
+          }))
+      });
+    }
+
+  },[]);
+
+
 
   const onSubmitCity = city => {
     let cityWeatherUrl = cityWeatherApi.replace('city_name', city)
@@ -35,6 +66,19 @@ function App() {
   function handleSubmit(event){
     event.preventDefault()
     onSubmitCity(city)
+  }
+
+
+
+  if(loading===true){
+    return   ( 
+      <div className="App">
+        <header className="App-header">
+          <h1>How is the weather ? </h1>
+          <div>Loading your current location wait...</div>
+        </header>
+      </div>
+    )
   }
 
 
